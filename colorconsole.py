5f70b8b174d4f3813bad89c0c3d952155df5575b
asherhe <35799436+asherhe@users.noreply.github.com>
2021-02-03 21:19:43 -0800

    Add updated code
    Added the old code by mistake

--- colorconsole.py
+++ colorconsole.py
@@ -1,49 +1,174 @@
-# Init colorama
-from colorama import init
+'''
+Implements basic colored-terminal I/O features
+'''
+
+# Import neccessary libraries
+from colorama import init, Fore, Back, Style
+import ctypes
+
+# Initialize console
 init()
 
-class ColorConsole():
-    def print(self, text, **kwargs):
-        '''
-        Prints a string at the current cursor position\n
-        **kwargs:\n
-        \t- fore: Text foreground in 3-bit RGB format (green is \"010\", for example)\n
-        \t- back: Text background in 3-bit RGB format
-        '''
-        fore = 7
-        back = 0
-        for key in kwargs:
-            val = kwargs[key]
-            if key == "fore":
-                fore = int(val[0]) + int(val[1])*2 + int(val[2])*4
-            elif key == "back":
-                back = int(val[0]) + int(val[1])*2 + int(val[2])*4
+# The 3-bit RGB codes corresponding to a color
+COLOR_CODE = {
+    "black"  : "000",
+    "red"    : "100",
+    "green"  : "010",
+    "yellow" : "110",
+    "blue"   : "001",
+    "magenta": "101",
+    "cyan"   : "011",
+    "white"  : "111",
+}
 
-        print("\033[1;3%d;4%dm%s" % (fore, back, text), end="")
+class ColorConsole:
+    '''
+    A console capable of printing text
+    '''
+    def __init__(self, title="ColorConsole Window"):
+        '''
+        Creates a new colored console
 
-    def printAt(self, text, row, col, **kwargs):
+        Parameters:
+        - `title`: The title of the console window
         '''
-        Prints a string at a specified position\n
-        **kwargs:\n
-        \t- fore: Text foreground in 3-bit RGB format (green is \"010\", for example)\n
-        \t- back: Text background in 3-bit RGB format
-        '''
-        self.moveCursor(row, col)
-        self.print(text, **kwargs)
+        ctypes.windll.kernel32.SetConsoleTitleW(title)
 
-    def moveCursor(self, row, col):
+    def print(self, val="", newline=False, **kwargs):
         '''
-        Moves the cursor to a certain position.\n
+        Prints an object at cursor position, moves cursor accordingly.
+
+        Parameters:
+        - `val`: The object to print
+        - `newline`: Whether a newline should be printed or not.
+        - `**kwargs`:
+            - `fgColor`: The color of the foreground. Must be one of:
+                - `\"black\"`
+                - `\"blue\"`
+                - `\"cyan\"`
+                - `\"green\"`
+                - `\"magenta\"`
+                - `\"red\"`
+                - `\"white\"`
+                - `\"yellow\"`
+            - `bgColor`: The color of the background. Must be one of:
+                - `\"black\"`
+                - `\"blue\"`
+                - `\"cyan\"`
+                - `\"green\"`
+                - `\"magenta\"`
+                - `\"red\"`
+                - `\"white\"`
+                - `\"yellow\"`
+            - `rgbBg`: A three-bit RGB value representing the background. Should be formatted `\"[Red][Green][Blue]\"`
+            - `rgbFg`: A three-bit RGB value representing the foreground. Should be formatted `\"[Red][Green][Blue]\"`
+
+        If both the RGB *and* the color name for the fore/background, the RGB value will be used.
         '''
-        print("\033[%d;%dH" % (row, col), end="")
+        # Set the foreground color
+        fg = "1001"
+        if "rgbFg" in kwargs:
+            fg = kwargs["rgbFg"]
+        elif "fgColor" in kwargs:
+            fg = COLOR_CODE[kwargs["fgColor"].lower()]
+
+        # Set the background color
+        bg = "1001"
+        if "rgbBg" in kwargs:
+            bg = kwargs["rgbBg"]
+        elif "bgColor" in kwargs:
+            bg = COLOR_CODE[kwargs["bgColor"].lower()]
+
+        # Create color escape codes
+        col = "\x1b[3%dm\x1b[4%dm" % (int(fg[::-1], 2), int(bg[::-1], 2))
+
+        # Replace newlines with non-color newlines
+        val = col + str(val).replace("\n", "\x1b[39m\x1b[49m\n" + col)
+        print(val, end="")
+
+        # Insert newline if trailing newline requested
+        if newline:
+            print()
+
+    def input(self, val="", **kwargs):
+        '''
+        Prints an object at cursor position, then asks for user input. moves cursor accordingly.
+
+        Parameters:
+        - `val`: The object to print
+        - `**kwargs`:
+            - `fgColor`: The color of the foreground. Must be one of:
+                - `\"black\"`
+                - `\"blue\"`
+                - `\"cyan\"`
+                - `\"green\"`
+                - `\"magenta\"`
+                - `\"red\"`
+                - `\"white\"`
+                - `\"yellow\"`
+            - `bgColor`: The color of the background. Must be one of:
+                - `\"black\"`
+                - `\"blue\"`
+                - `\"cyan\"`
+                - `\"green\"`
+                - `\"magenta\"`
+                - `\"red\"`
+                - `\"white\"`
+                - `\"yellow\"`
+            - `rgbBg`: A three-bit RGB value representing the background. Should be formatted `\"[Red][Green][Blue]\"`
+            - `rgbFg`: A three-bit RGB value representing the foreground. Should be formatted `\"[Red][Green][Blue]\"`
+
+        If both the RGB *and* the color name for the fore/background, the RGB value will be used.
+        '''
+        self.print(val, **kwargs)
+        self.print()
+        userInput = input()
+        return userInput
+
+    def setCursor(self, row=0, col=0):
+        '''
+        Sets cursor position.
+
+        Parameters:
+        - `row`: The row to set the cursor position (row 1 is the top)
+        - `col`: The column to set the cursor position (column 0 is the leftmost column)
+        '''
+        print("\x1b[%d;%dH" % (row, col), end="")
 
     def clear(self):
         '''
         Clears the output console
         '''
-        print("\033[H\033[J")
+        print("\033[H\033[J", end="")
 
 if __name__ == "__main__":
-    console = ColorConsole()
-    console.print("Hello,", fore="010")
-    console.printAt("World!", 2, 2, fore="011")
+    c = ColorConsole()
+    c.print("R", rgbBg="100")
+    c.print("A", rgbBg="110")
+    c.print("I", rgbBg="010")
+    c.print("N", rgbBg="011")
+    c.print("B", rgbBg="001")
+    c.print("O", rgbBg="101")
+    c.print("W", rgbBg="100", newline=True)
+    if c.input("Did you see any color?\n")[0].lower() == "y":
+        c.print("You're all ")
+        c.print("good to go!", fgColor="green")
+        c.setCursor(5, 1)
+        c.print("Press ")
+        c.print("Enter", rgbFg="000", bgColor="white")
+        c.input(" to continue...")
+        c.clear()
+        c.print("a")
+        for i in range(1, 10):
+            c.setCursor(i, i)
+            c.print("(%d, %d)" % (i, i))
+        c.print("\nTesting cursor positioning...", True, fgColor="cyan")
+        c.print("Did cursor positioning work?", fgColor="yellow")
+        c.print(" (In other words, the text above should be starting at the ")
+        c.print("coordinates", fgColor="magenta")
+        if c.input(" shown)\t")[0].lower() == "y":
+            c.print("Awesome! It all works so far!", True, fgColor="Green")
+        else:
+            c.print("Then it looks like you're having some problems. Try installing Colorama, or reinstalling it if you have it.")
+    else:
+        c.print("Then it looks like you're having some problems. Try installing Colorama, or reinstalling it if you have it.")
